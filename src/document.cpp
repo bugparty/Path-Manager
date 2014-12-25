cee83cadafb6d34f84976cfa3d5484dd553909ca
bowman_han <fancycode@gmail.com>
2014-12-25 20:30:44 +0800

    document passed test

--- src/document.cpp
+++ src/document.cpp
@@ -4,24 +4,54 @@
 BOOL parse(TCHAR* in, path_item* out){
 	long start=0;
 	long lsize = lstrlen(in);
-	memset(out, 0, sizeof(path_item));
-	out->index = 0;
+	
 	long index = 0;
 	BOOL onError=false;
-	for(int i=1;i < lsize;i++){
+	for (int i = 0; i < lsize;i++){
 		if(*(in+i) == _T(';')){
-			if(i-start > 0){
+			//process when i pointing to a ';'
+			if(i-start > 1){
+				memset(out, 0, sizeof(path_item));
 				out->index = index++;
-				out->path = (TCHAR *)malloc((i-start)*sizeof(TCHAR));
-				memcpy(out->path,(void *)(start+1),i-start-1);
-				*(out->path+i-start)=_T('\0');
-				out->next = (path_item *)malloc(sizeof(path_item));
-				memset(out->next, 0, sizeof(path_item));
+				out->path = (TCHAR *)malloc((i-start+1)*sizeof(TCHAR));
 				
-				out = out->next;
-				start = i;
+				memcpy(out->path,(void *)(in+start),(i-start+1)*sizeof(TCHAR));
+				*(out->path+i-start+1)=_T('\0');
+				//when processing the end of a line
+				if (i == lsize - 1){
+					out->next = NULL;
+				}
+				else{
+					out->next = (path_item *)malloc(sizeof(path_item));
+					memset(out->next, 0, sizeof(path_item));
+
+					out = out->next;
+					start = i + 1;
+				}
+				
+				
 			}
+			else
+			{
+				start = i + 1;
+			}
+			
+
 		}
+		//process the last line without an semicolon
+		else if (i == lsize - 1){
+			if (i - start > 0){
+				memset(out, 0, sizeof(path_item));
+				out->index = ++index;
+				out->path = (TCHAR *)malloc((i - start+2)*sizeof(TCHAR));
+
+				memcpy(out->path, (void *)(in+start ), (i - start + 1)*sizeof(TCHAR));
+				*(out->path + i - start+1) = _T('\0');
+			}
+
+		}
+		
+		
 	}
 		
 	return(!onError) ;
